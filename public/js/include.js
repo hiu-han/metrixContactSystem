0db21a6e5b14d5b0c03b27e181daded1903d867b
Heejae Han <ariados104@gmail.com>
2023-08-14 13:28:37 +0900

    230814 월 1327 - 컨택시스템 초기 구조 마크업 진행 중. - 진행 상황 정리 필요. - 기초 레이아웃 필요.

--- public/js/include.js
+++ public/js/include.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-  $('#header').load('./include/header.html');
+  $('#header').load('/include/header.html');
+  // $('#contactHeader').load('../include/contactHeader.html');
   $('#footer').load('./include/footer.html');
+  $('#foldedFooter').load('../include/foldedFooter.html');
   $('#aside').load('./include/aside.html');
 });
