35def0c0866fe1dae29ebcdf43343015db2629de
Heejae Han <ariados104@gmail.com>
2023-09-27 06:46:19 +0900

    230927 수 0646

--- public/js/include.js
+++ public/js/include.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $('#header').load('/include/header.html');
-  // $('#contactHeader').load('../include/contactHeader.html');
   $('#footer').load('./include/footer.html');
-  $('#foldedFooter').load('../include/foldedFooter.html');
   $('#aside').load('./include/aside.html');
 });
