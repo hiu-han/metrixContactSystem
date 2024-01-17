03c7a4613856a46177f68ef47101e59e7737c257
Heejae Han <ariados104@gmail.com>
2024-01-17 16:56:50 +0900

    240117 수 1656

--- public/js/include.js
+++ public/js/include.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  // $('#header').load('/include/header.html');
-  // $('#footer').load('/include/footer.html');
-  // $('#aside').load('/include/aside.html');
-  $('#header').load(
-    'https://hiu-han.github.io/metrixContactSystem/include/header.html'
-  );
-  $('#footer').load(
-    'https://hiu-han.github.io/metrixContactSystem/include/footer.html'
-  );
-  $('#aside').load(
-    'https://hiu-han.github.io/metrixContactSystem/include/aside.html'
-  );
+  $('#header').load('/include/header.html');
+  $('#footer').load('/include/footer.html');
+  $('#aside').load('/include/aside.html');
+  // $('#header').load(
+  //   'https://hiu-han.github.io/metrixContactSystem/include/header.html'
+  // );
+  // $('#footer').load(
+  //   'https://hiu-han.github.io/metrixContactSystem/include/footer.html'
+  // );
+  // $('#aside').load(
+  //   'https://hiu-han.github.io/metrixContactSystem/include/aside.html'
+  // );
 });
